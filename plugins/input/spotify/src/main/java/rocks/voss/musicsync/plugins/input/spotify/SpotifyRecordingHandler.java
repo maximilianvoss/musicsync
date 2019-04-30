2fb10d8ab1b753d1643bd13eb07dbb9a050871c4
Maximilian Voss <voss@adobe.com>
2019-04-30 22:57:40 +0200

    In which our hero introduces file validation

--- plugins/input/spotify/src/main/java/rocks/voss/musicsync/plugins/input/spotify/SpotifyRecordingHandler.java
+++ plugins/input/spotify/src/main/java/rocks/voss/musicsync/plugins/input/spotify/SpotifyRecordingHandler.java
@@ -3,9 +3,14 @@ package rocks.voss.musicsync.plugins.input.spotify;
 import com.wrapper.spotify.model_objects.specification.PlaylistTrack;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
+import org.tritonus.share.sampled.file.TAudioFileFormat;
 
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 public class SpotifyRecordingHandler {
     final private static Logger log = Logger.getLogger(SpotifyRecordingHandler.class.getName());
@@ -13,28 +18,72 @@ public class SpotifyRecordingHandler {
     public static void recordTrack(SpotifyHandler spotifyHandler, PlaylistTrack track, String filename)
             throws IOException, InterruptedException {
         new File(spotifyHandler.getCachePath()).mkdirs();
-        File dir = new File(spotifyHandler.getCachePath());
+
+        if (isFileValid(track, spotifyHandler.getCachePath(), filename)) {
+            log.debug("File is valid: " + filename);
+        } else {
+            log.debug("File is not valid: " + filename);
+            downloadFile(spotifyHandler, track, filename);
+        }
+    }
+
+    private static void downloadFile(SpotifyHandler spotifyHandler, PlaylistTrack track, String filename) throws InterruptedException, IOException {
+        log.info("Downloading: " + track.getTrack().getUri());
+        Runtime rt = Runtime.getRuntime();
+        StringBuilder command = new StringBuilder();
+        command.append("stream_recorder.pl --uri '")
+                .append(track.getTrack().getUri())
+                .append("' --silent --format mp3 --outdir ")
+                .append(spotifyHandler.getCachePath())
+                .append(" --filename ")
+                .append(filename);
+
+        String[] commands = {"/bin/bash", "-c", command.toString()};
+        log.debug("Executing: " + command.toString());
+        Process proc = rt.exec(commands);
+        proc.waitFor();
+        log.debug("Execution done");
+    }
+
+    private static boolean isFileValid(PlaylistTrack track, String path, String filename) {
+        File dir = new File(path);
         File[] files = dir.listFiles((directory, dirFile) -> StringUtils.equals(dirFile, filename));
 
         if (files != null && files.length == 0) {
-            log.debug("File does not exist: " + filename);
-            log.info("Downloading: " + track.getTrack().getUri());
-            Runtime rt = Runtime.getRuntime();
-            StringBuilder command = new StringBuilder();
-            command.append("stream_recorder.pl --uri '")
-                    .append(track.getTrack().getUri())
-                    .append("' --silent --format mp3 --outdir ")
-                    .append(spotifyHandler.getCachePath())
-                    .append(" --filename ")
-                    .append(filename);
+            try {
+                AudioFileFormat fileFormat = AudioSystem.getAudioFileFormat(files[0]);
+                if (fileFormat instanceof TAudioFileFormat) {
+                    Map<?, ?> properties = ((TAudioFileFormat) fileFormat).properties();
+                    String key = "duration";
+                    Long microseconds = (Long) properties.get(key);
+                    int mili = (int) (microseconds / 1000);
+                    int delta = mili - track.getTrack().getDurationMs();
 
-            String[] commands = {"/bin/bash", "-c", command.toString()};
-            log.debug("Executing: " + command.toString());
-            Process proc = rt.exec(commands);
-            proc.waitFor();
-            log.debug("Execution done");
-        } else {
-            log.debug("File does exist: " + filename);
+                    log.debug("MP3 duration  : " + mili);
+                    log.debug("Track duration: " + track.getTrack().getDurationMs());
+                    log.debug("Delta duration: " + delta);
+
+                    if (delta > 1000 || delta < -1000) {
+                        log.info("Delta is too big");
+                        files[0].delete();
+                        return false;
+                    }
+                } else {
+                    log.info("File is not okay");
+                    files[0].delete();
+                    return false;
+                }
+            } catch (IOException e) {
+                log.error("IOException", e);
+                files[0].delete();
+                return false;
+            } catch (UnsupportedAudioFileException e) {
+                log.error("UnsupportedAudioFileException", e);
+                files[0].delete();
+                return false;
+            }
         }
+        log.info("File is okay");
+        return true;
     }
 }
